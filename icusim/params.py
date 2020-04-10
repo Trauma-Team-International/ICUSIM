a30235ac638f5c279df2f85d525cd96d9ff23d88
Mikko Kotila <mailme@mikkokotila.com>
2020-04-10 20:42:44 +0300

    Update params.py

--- icusim/params.py
+++ icusim/params.py
@@ -12,34 +12,61 @@ def params(show_params=False):
     import random
     import numpy as np
         
-    initial_patient_count = 120
+    initial_patient_count =120
     days_to_simulate = 50
+    capacity_min = 200
+    capacity_max = 1000
+    ventilated_icu_factor = .5
+    fatality_rate_min = .2
+    fatality_rate_max = .6
+    ventilated_cfr_min = 1.3
+    ventilated_cfr_max = 1.7
+    standard_duration_min = 8
+    standard_duration_max = 25
+    ventilated_duration_factor_min = 1
+    ventilated_duration_factor_max = 1.2
+    doubles_in_days_min = 2.0
+    doubles_in_days_max = 12.0
+    ventilation_rate_min = .3
+    ventilation_rate_max = .8
     
-    _random_normal_ = (np.random.normal(1, 0.01)
+    # change scale for stronger random effect (only applies to normally distributed)
+    _random_normal_ = np.random.normal(loc=1, scale=0.01)
+    _random_normal_
     
     # set max capacity 
-    _capacity_ = random.choice(list(range(200, 1000, 50)))
+    _capacity_ = random.choice(list(range(capacity_min, capacity_max, step=50)))
     
     # set the fraction of ventilation capacity
-    _ventilated_capacity_ = (np.random.normal(1, 0.01) * .5)
+    _ventilated_capacity_ = (_random_normal_ * ventilated_icu_share)
     
     # set standard case fatality rate
-    _fatality_rate_ = round(random.choice(np.arange(0.2, 0.6, .01)), 2)
+    _fatality_rate_ = random.choice(np.arange(fatality_rate_min, fatality_rate_max, step=.01))
     
     # set ventilated case fatality rate
-    _ventilated_fatality_factor_ = random.choice(np.arange(1.3, 1.7, 0.01))
+    _ventilated_fatality_factor_ = random.choice(np.arange(ventilated_cfr_min, ventilated_cfr_max, step=0.01))
     
+    # set the mean duration of standard ICU stay (ventilated is based on it
+    _standard_icu_stay_duration_ = random.choice(range(standard_duration_min, standard_duration_max, step=1))
+                       
     # set a multiplier for ventilated stay duration
-    _ventilated_duration_factor_ = random.choice(np.arange(1, 1.2, 0.01))
+    _temp_ = np.arange(ventilated_duration_factor_min, ventilated_duration_factor_max, step=0.01)                   
+    _ventilated_duration_factor_ = random.choice(_temp_)
     
-    # initialize values
-    doubles_in_days = random.choice(np.arange(2.0, 12.0, .1))
-    require_ventilation_rate = random.choice(np.arange(.3, .8, .01))
+    # set the number of days during which doubling of cases happens
+    _doubles_in_days_ = random.choice(np.arange(doubles_in_days_min, doubles_in_days_max, step=.1))
+    
+    # set the percent of patients that require ventilation
+    _require_ventilation_rate_ = random.choice(np.arange(ventilation_rate_min, ventilation_rate_max, step=.01))
+   
+    # review the logic here
+    doubles_in_days = _doubles_in_days_
+    require_ventilation_rate = _require_ventilation_rate_
     standard_icu_capacity = _capacity_ * (1 - _ventilated_capacity_)
     ventilated_icu_capacity = _capacity_ * _ventilated_capacity_
     standard_icu_fatality_rate = _fatality_rate_
     ventilated_icu_fatality_rate = _fatality_rate_ * _random_normal_ * _ventilated_fatality_factor_
-    standard_icu_stay_duration = int(random.choice(range(8, 25, 1)))
+    standard_icu_stay_duration = _standard_icu_stay_duration_
     ventilated_icu_stay_duration = standard_icu_stay_duration * _random_normal_ * _ventilated_duration_factor_
     
     # generates the input parameters

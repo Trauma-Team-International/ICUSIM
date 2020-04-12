c39a3dc83b1721bfedf76341745787e97d267a0e
Mikko Kotila <mikko@pop-os.localdomain>
2020-04-12 19:12:50 +0300

    add Monte Carlo method

--- tests.py
+++ tests.py
@@ -1,6 +1,25 @@
-from icusim.icu_burden_simulator import simulate
-from icusim.stats_to_dataframe import stats_to_dataframe
-from icusim.params import params
+import icusim
 
-out = simulate(params())
-df = stats_to_dataframe(out)
+out = icusim.simulate(icusim.params())
+df = icusim.stats_to_dataframe(out)
+
+params = {'initial_patient_count': 80,
+          'days_to_simulate': 50,
+          'total_capacity_min': 200,
+          'total_capacity_max': 1000,
+          'ventilated_icu_share_min': .4,
+          'ventilated_icu_share_max': .6,
+          'standard_cfr_min': 0.2,
+          'standard_cfr_max': 0.6,
+          'ventilated_cfr_min': 1.3,
+          'ventilated_cfr_max': 1.7,
+          'standard_duration_min': 8.5,
+          'standard_duration_max': 25.5,
+          'ventilated_duration_factor_min': .9,
+          'ventilated_duration_factor_max': 1.1,
+          'doubles_in_days_min': 2.0,
+          'doubles_in_days_max': 12.0,
+          'ventilation_rate_min': 0.3,
+          'ventilation_rate_max': 0.8}
+
+icusim.MonteCarlo(10, params)

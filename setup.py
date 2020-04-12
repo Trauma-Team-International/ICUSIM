7c2d8b8c75259a7797be4a00c997d55ddfdc9170
Mikko Kotila <mikko@pop-os.localdomain>
2020-04-12 19:59:41 +0300

    add Sobol Sensitivity method

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ MAINTAINER_EMAIL = 'mailme@mikkokotila.com'
 URL = 'http://autonom.io'
 LICENSE = 'MIT'
 DOWNLOAD_URL = 'https://github.com/autonomio/ICUSIM/'
-VERSION = '0.0.2'
+VERSION = '0.2.0'
 
 try:
     from setuptools import setup
